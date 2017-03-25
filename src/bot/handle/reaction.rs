d89df98336cfcc383b8c92f0613e2a527651b4e7
oberien <jaro.fietz@gmx.de>
2017-03-25 15:10:07 +0100

    ref(config): Clean up log messages

--- src/bot/handle/reaction.rs
+++ src/bot/handle/reaction.rs
@@ -11,8 +11,11 @@ impl Bot {
     pub fn handle_reaction_add(&self, reaction: Reaction) -> Result<()> {
         let server = self.server_by_channel(reaction.channel_id)?;
         let message = server.messages.get(&reaction.message_id);
+        let user = unwrap!(server.members.get(&reaction.user_id)).clone();
+        let channel = unwrap!(server.channels.get(&reaction.channel_id)).clone();
         let mut map = reaction.into_map()?;
-        // TODO: improve infos in map with channel and user
+        user.merge_into_map_prefix(&mut map, "user_")?;
+        channel.merge_into_map_prefix(&mut map, "channel_")?;
         if let Some(msg) = message {
             msg.clone().merge_into_map_prefix(&mut map, "message_")?;
             let template = server.config.as_ref().and_then(|c| c.reaction_add_cached_msg.as_ref());
@@ -27,8 +30,11 @@ impl Bot {
     pub fn handle_reaction_remove(&self, reaction: Reaction) -> Result<()> {
         let server = self.server_by_channel(reaction.channel_id)?;
         let message = server.messages.get(&reaction.message_id);
+        let user = unwrap!(server.members.get(&reaction.user_id)).clone();
+        let channel = unwrap!(server.channels.get(&reaction.channel_id)).clone();
         let mut map = reaction.into_map()?;
-        // TODO: improve infos in map with channel and user
+        user.merge_into_map_prefix(&mut map, "user_")?;
+        channel.merge_into_map_prefix(&mut map, "channel_")?;
         if let Some(msg) = message {
             msg.clone().merge_into_map_prefix(&mut map, "message_")?;
             let template = server.config.as_ref().and_then(|c| c.reaction_remove_cached_msg.as_ref());
